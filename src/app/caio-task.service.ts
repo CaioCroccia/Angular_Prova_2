79623023a13e638a2ac1f5aab37ca16fe13af6e6
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-06-17 18:32:12 -0300

    1 file modified

--- src/app/caio-task.service.ts
+++ src/app/caio-task.service.ts
@@ -13,10 +13,21 @@ export class CaioTaskService {
     {
       periodo: 'manha',
       periodo: 'tomar cafe',
-
     }
+
 
   ]
   constructor() { }
+   
+  ShowList(){
+    return this.list;
+  }
 
+  PutList(periodo: string, tarefa: string){
+    this.list.push({periodo, tarefa})
+  }
+
+  deleteList(index: number){
+    this.list.slice(index,1);
+  }
 }
