c3d5e29d766cfda1b26a173d86cbafb626fdb717
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-06-17 18:26:39 -0300

    1 file modified

--- src/app/caio-task.service.ts
+++ src/app/caio-task.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 
 interface Tasks{
-  
+  periodo: string;
+  tarefa: string
 }
 
 
 
 @Injectable()
 export class CaioTaskService {
+  list: Array<Tasks> = [
+    {
+      periodo: 'manha',
+      periodo: 'tomar cafe',
 
+    }
+
+  ]
   constructor() { }
 
 }
