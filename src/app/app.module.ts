019cd0ed46801457282545e4de5c400fbaf47192
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-06-17 18:12:08 -0300

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CaioHomeComponent } from './caio-home/caio-home.component';
+import { CaioTasksComponent } from './caio-tasks/caio-tasks.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, CaioHomeComponent, CaioTasksComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
